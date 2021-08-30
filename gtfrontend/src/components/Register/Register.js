a6690bcbf22f4d11f985a49c26a8fd95f53e0637
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-08-30 19:55:54 +0100

    finished layout and added styling

--- gtfrontend/src/components/Register/Register.js
+++ gtfrontend/src/components/Register/Register.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import StyledRegister from './Register.styled'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
+
 
 
 const Register = () => {
@@ -12,7 +13,7 @@ const Register = () => {
     first_name:'',
     last_name:'',
     email:'',
-    occupation:'',
+    occupation:'Choose your occupation',
     password:'',
     password_confirmation:'',
     remember_token: false  
@@ -23,7 +24,6 @@ const Register = () => {
       return {
         ...prevState,
         [e.target.name]: e.target.value,
-        remember_token: e.target.checked
       };
     });
   };  
@@ -31,42 +31,44 @@ const Register = () => {
   const signUp = async (e) => {
     e.preventDefault();
     console.log(userData);
-    console.log();
-    // try {
-    //   await axios.post("http://localhost:8000/api/register", userData)
-    //   .then(response => {
-    //     console.log(response);
-    //   });
-    // } catch (error) {
-    //   console.log(error);
-    // };
+    try {
+      await axios.post("http://localhost:8000/api/register", userData)
+      .then(response => {
+        console.log(response);
+      });
+    } catch (error) {
+      console.log(error);
+    };
   };
 
   return (
-    <StyledRegister>
+    <StyledRegister className="m-auto col-sm-6 p-4 mt-5">
+      <div className="container m-auto">
       <h1 className="pb-3">Register</h1>
-      <div className="container m-auto col-sm-4">
         <form action="">
           <div className="form-group">
             <label>First Name:</label>
-            <input type="text" name="first_name" onChange={e=>handleChange(e)} className="form-control my-2 "/>
+            <input type="text" name="first_name" onChange={e=>handleChange(e)} placeholder="Gareth" className="form-control my-2 "/>
           </div>
           <div className="form-group">
             <label>Last Name</label>
-            <input type="text" name="last_name" onChange={e=>handleChange(e)} className="form-control my-2 "/>
+            <input type="text" name="last_name" onChange={e=>handleChange(e)} placeholder="Webster" className="form-control my-2 "/>
           </div>
           <div className="form-group">
             <label>Email</label>
-            <input type="email" name="email" onChange={e=>handleChange(e)} className="form-control my-2 "/>
+            <input type="email" name="email" onChange={e=>handleChange(e)} placeholder="gareth@webster.com" className="form-control my-2 "/>
           </div>
           <div className="form-group">
             <label>Occupation</label>
-              <select name="occupation" id="occupation" onChange={e=>handleChange(e)}>
-                <option value="OA / Support">OA / Support</option>
+              <select className="form-select" name="occupation" defaultValue={userData.occupation} onChange={e=>handleChange(e)}>
+                <option value={userData.occupation} disabled hidden >Choose your occupation</option>
+                <option value="Oa / Support">OA / Support</option>
                 <option value="Dispensing Optician">Dispensing Optician</option>
                 <option value="Manager">Manager</option>
                 <option value="Optometrist">Optometrist</option>
-              </select>
+                <option value="Admin">Admin</option>
+                <option value="HC assistant">HC assistant</option>
+              </select> 
           </div>
           <div className="form-group">
             <label>Password</label>
@@ -76,11 +78,7 @@ const Register = () => {
             <label>Confirm Password</label>
             <input type="password" name="password_confirmation" onChange={e=>handleChange(e)} className="form-control my-2 "/>
           </div>
-          <div className="form-group">
-            <input type="checkbox" name="remember_token" id="token" checked={userData.remember_token} onChange={e=>handleChange(e)} />
-            <label htmlFor="token">remember me</label>
-          </div>
-          <button type="submit" onClick={signUp} className="btn btn-primary">Sign In</button>
+          <button type="submit" onClick={signUp} className="btn btn-warning px-4 py-1">Register</button>
         </form>
       </div>
     </StyledRegister>
--- gtfrontend/src/components/Register/Register.styled.js
+++ gtfrontend/src/components/Register/Register.styled.js
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
 
 const StyledRegister = styled.div`
+  background-color: #6fc77a ;
+  border-radius:0.5rem;
+  form{
+    font-size: large;
+  }
+  * .form-group label {
+    // padding: 0.5rem 0;
+  }
 `
 
 export default StyledRegister;
