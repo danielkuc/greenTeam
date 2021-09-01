7fe0f0c48008d779ec081b0bf4bc36b4f6f6ac5e
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-09-01 08:15:28 +0100

    cleaned up dead code and added comments

--- gtfrontend/src/components/Register/Register.js
+++ gtfrontend/src/components/Register/Register.js
@@ -4,43 +4,6 @@ import StyledRegister from './Register.styled';
 import * as Yup from 'yup';
 
 const Register = () => {
-  // validation function - must return an object which
-  // keys must be symmetrical to values/initialValues
-  // const validate = values =>{
-  //   const errors = {};
-  //   if (!values.first_name) {
-  //     errors.first_name = 'First name required';
-  //   } else if (values.first_name.length < 3) {
-  //     errors.first_name = 'Must be at least 3 characters'
-  //   }
-    
-  //   if (!values.last_name) {
-  //     errors.last_name = 'Last name required';
-  //   } else if (values.last_name.length < 3) {
-  //     errors.last_name = 'Must be at least 3 characters'
-  //   }
-
-  //   if (!values.email) {
-  //     errors.email = 'Email required';
-  //   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-  //     errors.email = 'Invalid email address';
-  //   }
-
-  //   if (!values.password) {
-  //     errors.password = 'Password required';
-  //   } else if (values.password.length < 3) {
-  //     errors.password = 'Must be at least 3 characters'
-  //   }
-
-  //   if (!values.password_confirmation) {
-  //     errors.password_confirmation = 'Password required';
-  //   } else if (values.password_confirmation.length < 3) {
-  //     errors.password_confirmation = 'Must be at least 3 characters'
-  //   }
-  //   return errors;
-  // };
-
-
   // initializing formik: passing state and submit.
   const formik = useFormik({
     initialValues:{
@@ -76,12 +39,8 @@ const Register = () => {
               <label htmlFor="first_name">First Name:</label>
               <input 
                 type="text" 
-                // name="first_name"
                 id="first_name"
                 {...formik.getFieldProps('first_name')}
-                // onChange={formik.handleChange}
-                // onBlur={formik.handleBlur}
-                // value={formik.values.first_name} 
                 className="form-control my-2 "
               />
               {formik.touched.first_name && formik.errors.first_name ? (<div className="error">{formik.errors.first_name}</div>) : null}
@@ -91,12 +50,8 @@ const Register = () => {
               <label htmlFor="last_name">Last Name</label>
               <input 
                 type="text" 
-                // name="last_name"
                 id="last_name"
                 {...formik.getFieldProps('last_name')}
-                // onChange={formik.handleChange}
-                // onBlur={formik.handleBlur}
-                // value={formik.values.last_name} 
                 className="form-control my-2 "
               />
               {formik.touched.last_name && formik.errors.last_name ? (<div className="error">{formik.errors.last_name}</div>) : null}
@@ -106,12 +61,8 @@ const Register = () => {
               <label htmlFor="email">Email</label>
               <input 
                 type="email" 
-                // name="email"
                 id="email"
                 {...formik.getFieldProps('email')}
-                // onChange={formik.handleChange}
-                // onBlur={formik.handleBlur}
-                // value={formik.values.email} 
                 className="form-control my-2 "
               />
               {formik.touched.email && formik.errors.email ? (<div className="error">{formik.errors.email}</div>) : null}
@@ -120,11 +71,8 @@ const Register = () => {
             <div className="form-group">
               <label htmlFor="occupation">Occupation</label>
                 <select 
-                  // name="occupation" 
                   id="occupation" 
                   {...formik.getFieldProps('occupation')}
-                  // onChange={formik.handleChange} 
-                  // value={formik.values.occupation}
                   className="form-select"
                   >
                   <option disabled hidden >Choose your occupation</option>
@@ -141,12 +89,8 @@ const Register = () => {
               <label htmlFor="password">Password</label>
               <input 
                 type="password" 
-                // name="password" 
                 id="password"
                 {...formik.getFieldProps('password')}
-                // onChange={formik.handleChange}
-                // onBlur={formik.handleBlur}
-                // value={formik.values.password}
                 className="form-control my-2 "
               />
               {formik.touched.password && formik.errors.password ? (<div className="error">{formik.errors.password}</div>) : null}
@@ -156,12 +100,8 @@ const Register = () => {
               <label htmlFor="password_confirmation">Confirm Password</label>
               <input 
                 type="password" 
-                // name="password_confirmation" 
                 id="password_confirmation"
                 {...formik.getFieldProps('password_confirmation')}
-                // onChange={formik.handleChange}
-                // onBlur={formik.handleBlur}
-                // value={formik.values.password_confirmation}
                 className="form-control my-2"
               />
               {formik.touched.password_confirmation && formik.errors.password_confirmation ? (<div className="error">{formik.errors.password_confirmation}</div>) : null}
