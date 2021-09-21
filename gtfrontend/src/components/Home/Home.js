23ad966665d2085e4c8e863699ae2a40129fec90
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-09-21 22:04:24 +0100

    added layout and placeholders in home component

--- gtfrontend/src/components/Home/Home.js
+++ gtfrontend/src/components/Home/Home.js
@@ -11,28 +11,72 @@ const Home = () => {
     },
     {
       headline: 'New operating procedures',
-      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quos vitae? Beatae praesentium, unde nobis aspernatur rem sequi magnam cum!'
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quos vitae? Beatae praesentium, unde nobis aspernatur rem sequi magnam cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, quos vitae? Beatae praesentium, unde nobis aspernatur rem sequi magnam cum!'
     },
     {
       headline:'Store updates',
-      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus dignissimos sit modi est at harum amet asperiores molestiae nemo, esse autem eaque mollitia quod omnis fugiat, voluptatum iusto officiis cumque! Cum, nulla impedit. Cupiditate nobis exercitationem, consequuntur recusandae error ducimus delectus quasi nisi veritatis at quaerat amet perferendis iste dolore deleniti repellendus nesciunt est odio. Nostrum expedita ipsam ducimus odio.'
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus dignissimos sit modi est at harum amet asperiores molestiae nemo, esse autem eaque mollitia quod omnis fugiat, voluptatum iusto officiis cumque! Cum, nulla impedit. Cupiditate nobis exercitationem, consequuntur recusandae error ducimus delectus quasi nisi veritatis at quaerat amet perferendis iste dolore deleniti repellendus nesciunt est odio.'
     }
   ];
 
+  const minor = [
+    {
+      head: 'Headline Placeholder #1',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nostrum, suscipit impedit dignissimos, dolor voluptatem voluptate quasi quibusdam minus amet fuga consectetur possimus a neque.'
+    },
+    {
+      head: 'Headline Placeholder #2',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni labore aliquid perspiciatis accusamus alias necessitatibus numquam architecto commodi deleniti. Optio suscipit aliquid corrupti exercitationem minus!'
+    },
+    {
+      head: 'Headline Placeholder #3',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum iusto corporis, ad hic voluptate reprehenderit obcaecati impedit necessitatibus in dicta magni, qui quas odio non?'
+    },
+    {
+      head: 'HeadlinePlaceholder #4',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus aliquam assumenda nobis explicabo nostrum architecto harum alias delectus, tempora hic quidem odit nam deserunt inventore.'
+    }
+  ];
 
   return (
     <CONTAINER fluid="sm">
-      <Row className="important">
+      <Row className="important justify-content-center">
+        <p className="h1 text-center pb-4">Key updates</p>
         {
-          important.map(item => {return(
-            <Col>
-              <CardHeader>
-                <Card.Title>
-                  {`${item.headline}`}
-                </Card.Title>
-              </CardHeader>
+          important.map((article, index) => {return(
+            <Col key={index} sm={10} lg={4}>
+            <Card>
+                <CardHeader>
+                  <Card.Title>
+                    {`${article.headline}`}
+                  </Card.Title>
+                </CardHeader>
+                <Card.Body>
+                  {`${article.body}`}
+                </Card.Body>
+              </Card>
             </Col>
           )})
+        }
+      </Row>
+      <Row className="secondary justify-content-center">
+        <p className="h2 text-center py-5">Other updates</p>
+        {
+          minor.map((article, index) => { return(
+            <Col key={index} sm={10} lg={3}>
+              <Card>
+                <CardHeader>
+                  <Card.Title>
+                    {`${article.head}`}
+                  </Card.Title>
+                </CardHeader>
+                <Card.Body>
+                  {`${article.body}`}
+                </Card.Body>
+              </Card>
+            </Col>
+            )
+          })
         }
       </Row>      
     </CONTAINER>
--- gtfrontend/src/components/Home/Home.styled.js
+++ gtfrontend/src/components/Home/Home.styled.js
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 import { Container } from 'react-bootstrap';
 
 const CONTAINER = styled(Container)`
-  
+  .card{
+    margin: 1rem 0; 
+  }
 `;
 
 export default CONTAINER;
