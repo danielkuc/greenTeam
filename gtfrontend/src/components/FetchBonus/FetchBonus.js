2686cee7e26cb929020436649cc75f5430b2e92f
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-11-06 16:31:20 +0000

    added accordion to fetchbonus component

--- gtfrontend/src/components/FetchBonus/FetchBonus.js
+++ gtfrontend/src/components/FetchBonus/FetchBonus.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as yup from 'yup';
+import { Formik } from 'formik'
+import { Accordion, Card, Row, Col,Button ,FloatingLabel, Form, FormControl } from 'react-bootstrap';
+import CONTAINER from './FetchBonus.styled';
+import apiClient from '../../services/api';
 
 const FetchBonus = () => {
+  const [isLodaing, setIsLoading] = useState(false);
+
+  const validator = yup.object({
+    username: yup.string(),
+    date: yup.date()
+  });
+
+  // ADD DATE RANGE FROM-TO
+
   return (
-    <div>
-      <p>Hello from fetch bonus</p>
-    </div>
+    <>
+      <CONTAINER>
+        <Accordion
+          defaultActiveKey="0"
+        >
+          <Accordion.Header>
+            Search parameters
+          </Accordion.Header>
+          <Accordion.Body>
+            <Formik
+              validationSchema={validator}
+              // onSubmit={handleSUbmit}
+              initialValues={{ 
+                username:'',
+                date: null
+               }}
+            >
+
+            </Formik>
+          </Accordion.Body>
+        </Accordion>
+      </CONTAINER>
+    </>
   )
 }
 
